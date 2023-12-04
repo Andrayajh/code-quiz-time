02352488f115e9bedd5889463f79460bdb3772dd
Dray Hey <puttyhill@Drays-MacBook-Pro.local>
2023-12-04 14:28:47 +0000

    writing js logic

---
+++ assets/assets/js/logic.js
@@ -0,0 +1,19 @@
+let startButton = document.getElementById("start")
+let questions = document.getElementById("questions")
+
+startButton.addEventListener("click", start)
+
+function startQuiz() {
+console.log("quiz started")
+startButton.classList.add("hide")
+
+
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
